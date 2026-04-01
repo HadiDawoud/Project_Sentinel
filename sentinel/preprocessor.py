1b160124a755d90b172299a706a60ab0fd353887
Hadi Dawoud <hadi.dawoud@iem.fraunhofer.de>
2026-04-01 19:31:03 +0200

    strip emojis because feelings don't classify themselves

--- sentinel/preprocessor.py
+++ sentinel/preprocessor.py
@@ -10,9 +10,20 @@ class TextPreprocessor:
         self.hashtag_pattern = re.compile(r'#\w+')
         self.email_pattern = re.compile(r'\S+@\S+\.\S+')
         self.whitespace_pattern = re.compile(r'\s+')
+        self.emoji_pattern = re.compile(
+            "["
+            "\U0001F600-\U0001F64F"
+            "\U0001F300-\U0001F5FF"
+            "\U0001F680-\U0001F6FF"
+            "\U0001F1E0-\U0001F1FF"
+            "\U00002702-\U000027B0"
+            "\U000024C2-\U0001F251"
+            "]+", flags=re.UNICODE
+        )
 
     def clean(self, text: str) -> str:
         text = text.lower()
+        text = self.emoji_pattern.sub(' ', text)
         text = self.url_pattern.sub(' ', text)
         text = self.mention_pattern.sub(' ', text)
         text = self.email_pattern.sub(' ', text)
